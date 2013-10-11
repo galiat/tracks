c141f0185c61d35d7ac63dc84789c3f8a9abcffc
galia <galiat@gmail.com>
2013-10-11 11:14:11 -0400

    tags_for_cloud => cloud.tags

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -9,7 +9,7 @@ class StatsController < ApplicationController
     cloud = TagCloud.new(current_user,@cut_off_3months)
     cloud.get_stats_tags
 
-    @tags_for_cloud = cloud.tags_for_cloud
+    @tags_for_cloud = cloud.tags
 
     @tags_min = cloud.tags_min
 
