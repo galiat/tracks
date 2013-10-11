43de14437f0abd3bb902270fc1c3fbc7f2a3774e
galia <galiat@gmail.com>
2013-10-11 10:30:20 -0400

    i3

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -552,6 +552,9 @@ class StatsController < ApplicationController
   end
 
   def get_stats_tags
+    cloud = TagCloud.new(current_user)
+    cloud.get_stats_tags
+    
     # tag cloud code inspired by this article
     #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
